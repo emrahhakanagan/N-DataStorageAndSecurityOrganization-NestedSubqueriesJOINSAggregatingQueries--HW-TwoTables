5173e4cfe412338ffcc0f404b49c961358fd4035
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-30 06:07:08 +0300

    initial commit

---
+++ src/main/groovy/com/agan/join/NestedSubQJoiNsAggregatingQHwTwoTablesApplication.groovy
@@ -0,0 +1,13 @@
+package com.agan.join
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NestedSubQJoiNsAggregatingQHwTwoTablesApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NestedSubQJoiNsAggregatingQHwTwoTablesApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/agan/join/NestedSubQJoiNsAggregatingQHwTwoTablesApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.agan.join
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NestedSubQJoiNsAggregatingQHwTwoTablesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
